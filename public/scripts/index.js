c17bbb92e4b490dd11c60332222385e0c5ca0e9b
Christina Sijera <132604188+siemesnn@users.noreply.github.com>
2025-07-25 15:52:20 +0800

    test

---
+++ public/scripts/index.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.tab').forEach(tab => {
+  tab.addEventListener('click', function() {
+    document.querySelectorAll('.tab').forEach(t => t.classList.remove('active'));
+    document.querySelectorAll('.tab-content').forEach(tc => tc.classList.remove('active'));
+    this.classList.add('active');
+    document.getElementById(this.dataset.tab).classList.add('active');
+  });
+});
